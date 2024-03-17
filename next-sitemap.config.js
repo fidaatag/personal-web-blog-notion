d36d3f17ce93693fc70fb59fab259e0ac8fefc29
fidaatag <fidaa2fidaa@gmail.com>
2024-03-17 22:56:27 +0700

    change url sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://im-fidaa.netlify.app',
+  siteUrl: process.env.SITE_URL || 'https://fidaatag.vercel.app/',
   generateRobotsTxt: true,
   autoLastmod: false,
   generateIndexSitemap: false,
